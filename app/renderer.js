6a64c01243c006334d53141adc0570db77f2459f
Han Han <haannn@qq.com>
2020-06-13 12:06:14 +0800

    feat: 拖动文件直接打开

--- app/renderer.js
+++ app/renderer.js
@@ -71,3 +71,39 @@ revertButton.addEventListener("click", () => {
   renderMarkdownToHtml(originalContent);
   updateUserInterface();
 });
+const getDraggedFile = (event) => event.dataTransfer.items[0];
+const getDroppedFile = (event) => event.dataTransfer.files[0];
+const fileTypeIsSupported = (file) => {
+  return ["text/plain", "text/markdown"].includes(file.type);
+};
+
+document.addEventListener("dragstart", (event) => event.preventDefault());
+document.addEventListener("dragover", (event) => event.preventDefault());
+document.addEventListener("dragleave", (event) => event.preventDefault());
+document.addEventListener("drop", (event) => event.preventDefault());
+
+markdownView.addEventListener("dragover", (event) => {
+  const file = getDraggedFile(event);
+
+  if (fileTypeIsSupported(file)) {
+    markdownView.classList.add("drag-over");
+  } else {
+    markdownView.classList.add("drag-error");
+  }
+});
+markdownView.addEventListener("dragleave", () => {
+  markdownView.classList.remove("drag-over");
+  markdownView.classList.remove("drag-error");
+});
+
+markdownView.addEventListener("drop", (event) => {
+  const file = getDroppedFile(event);
+
+  if (fileTypeIsSupported(file)) {
+    mainProcess.openFile(currentWindow, file.path);
+  } else {
+    alert("That file type is not suported");
+  }
+  markdownView.classList.remove("drag-over");
+  markdownView.classList.remove("drag-error");
+});
